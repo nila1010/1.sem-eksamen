f945c3913f10d87468b0ebeab209a51b919531a7
NL <nila0001@stud.kea.dk>
2023-06-12 14:37:46 +0200

    oprydning billeder

--- billede.js
+++ billede.js
@@ -2,12 +2,12 @@ document.querySelector("#nicolai").addEventListener("mouseover", hover);
 
 function hover() {
   console.log("hover");
-  document.querySelector("#nicolai").src = "imgs/index/baggrundcss2.png";
+  document.querySelector("#nicolai").src = "imgs/index/baggrundcss2.webp";
 }
 
 document.querySelector("#nicolai").addEventListener("mouseout", out);
 
 function out() {
   console.log("hover");
-  document.querySelector("#nicolai").src = "imgs/index/baggrundhtml.png";
+  document.querySelector("#nicolai").src = "imgs/index/baggrundhtml.webp";
 }
